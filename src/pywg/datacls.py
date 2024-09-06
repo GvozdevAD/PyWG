977304c395d8620ecc08737e9d0254df66514abc
GvozdevAD <alexander@gvozdev.net>
2024-09-06 17:02:50 +0300

    add PeerInfo, ConfigInfo and fix InterfaceInfo

--- src/pywg/datacls.py
+++ src/pywg/datacls.py
@@ -6,21 +6,34 @@ class Keys:
     public: str
     private: str
 
+
 @dataclass(slots=True, frozen=True)
-class ConfigInfo:
-    path_to_config: Path
-    address: str
-    postup: str
-    postdown: str
-    listenport: str
-    privatekey: str
+class InterfaceInfo:
+    PathToConfig: Path
+    Address: str
+    PostUp: str
+    PostDown: str
+    ListenPort: str
+    PrivateKey: str
 
     def __str__(self) -> str:
         return (
             f"Path: {self.path_to_config}\n"
-            f"Address {self.address}\n"
-            f"PostUp {self.postup}\n"
-            f"PostDown {self.postdown}\n"
-            f"ListenPort {self.listenport}\n"
-            f"PrivateKey {self.privatekey}\n"
+            f"Address {self.Address}\n"
+            f"PostUp {self.PostUp}\n"
+            f"PostDown {self.PostDown}\n"
+            f"ListenPort {self.ListenPort}\n"
+            f"PrivateKey {self.PrivateKey}\n"
         )
+
+
+@dataclass(slots=True, frozen=True)
+class PeerInfo:
+    title: str
+    PublicKey: str
+    AllowedIps: str
+
+@dataclass(slots=True, frozen=True)
+class ConfigInfo:
+    interface: InterfaceInfo
+    peers: list[PeerInfo]
